0008fa4488adca2252ebb8fe7f3ba05456ee9237
amanojs <takashivue@gmail.com>
2022-02-04 13:27:03 +0900

    fix(RegisterForm): RegisterFormのデザイン微修正
    入力フォームにアイコンを追加。スマホ画面のカードをフル表示に切り替え。

--- src/pages/user/register.tsx
+++ src/pages/user/register.tsx
@@ -3,7 +3,7 @@ import RegisterForm from 'components/user/register/RegisterForm';
 
 const register: NextPage = () => {
     return (
-        <div className="flex justify-center items-center p-5 min-h-screen h-full bg-slate-700">
+        <div className="flex justify-center items-center md:p-5 min-h-screen h-full bg-slate-700">
             <RegisterForm />
         </div>
     );
